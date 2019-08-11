cfc93278d690879e00cbdc9d926c7b7a297f678d
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-08-11 23:53:04 +0600

    main file

---
+++ extension.js
@@ -0,0 +1,30 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+
+
+function activate(context) {
+  let compfyCleanup = null;
+	
+
+	context.subscriptions.push(compfyCleanup);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
